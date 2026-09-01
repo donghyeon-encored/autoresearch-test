dfdc20c4cc5928a4f6a8b91101849813a45211b4
김동현 <dhkim@encoredtech.com>
2026-09-01 10:39:10 +0900

    revert: restore mask rate 0.6 (0.75 regressed)

--- train.py
+++ train.py
@@ -167,7 +167,7 @@ GRAD_CLIP = 1.0
 # Batching
 DEVICE_BATCH_SIZE = 16
 GRAD_ACCUM_STEPS = 4
-MASK_RATE = 0.75
+MASK_RATE = 0.6
 
 # ---------------------------------------------------------------------------
 # Setup: data, model, optimizer, dataloader
